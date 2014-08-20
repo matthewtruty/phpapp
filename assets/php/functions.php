4e2f9da65ef3188f4d4428e609793e7715308959
Matthew Truty <mdtruty@gmail.com>
2014-08-20 11:31:46 -0400

    Function update

--- assets/php/functions.php
+++ assets/php/functions.php
@@ -5,19 +5,19 @@
 
 function init(){
 
-    $page = get_page();
+   echo  $page = get_page();
     #maps a file to a particular page on the site
     switch ($page) {
         case "index.php":
             include("content/pages/home.php");
         case "blue":
-            echo "Your favorite color is blue!";
+            echo "Your favorite color is bluddde!";
             break;
         case "green":
             echo "Your favorite color is green!";
             break;
         default:
-            
+
     }
 
 
