30fe7a9d336b4aa24e852c20ee2af1f6667de17a
Matthew Truty <mdtruty@gmail.com>
2014-08-20 11:15:18 -0400

    Quote errore

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     include("common/nav.php"); //loads site navigation
     include("common/init.php"); //loads site content
     include("common/footer.php"); //include the closing tags and JS files
- echo '<div class='container'>
+ echo '<div class="container">
 TEST HOME
 </div>';
 ?>
