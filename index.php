7630be22bab072440914fa229075a0f9fdc3c401
Matthew Truty <mdtruty@gmail.com>
2014-08-20 07:56:31 -0400

    Added DB connection string

--- index.php
+++ index.php
@@ -7,3 +7,20 @@
  */
 
 echo "Hello World - PHP";
+
+
+
+
+  $connectionInfo = array("UID" => "mtruty@xy70sqt4b9", "pwd" => "JMJpray4MDT$$", "Database" => "phpapp", "LoginTimeout" => 30, "Encrypt" => 1);
+  $serverName = "tcp:xy70sqt4b9.database.windows.net,1433";
+  $conn = sqlsrv_connect($serverName, $connectionInfo);
+
+
+
+if( $conn ) {
+     echo "Connection established.<br />";
+}else{
+     echo "Connection could not be established.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
+?>
