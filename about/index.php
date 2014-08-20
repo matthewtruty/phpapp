4ed4e57cc34f1d7b0833578d83b50639ca5cc5f4
Matthew Truty <mdtruty@gmail.com>
2014-08-20 11:37:49 -0400

    Reference update

--- about/index.php
+++ about/index.php
@@ -1,5 +1,5 @@
 <?php
-include("common/header.php"); //include header info (DB,Title,etc)
-include("common/nav.php"); //loads site navigation
-include("common/init.php"); //loads site content
-include("common/footer.php"); //include the closing tags and JS files
+include("../common/header.php"); //include header info (DB,Title,etc)
+include("../common/nav.php"); //loads site navigation
+include("../common/init.php"); //loads site content
+include("../common/footer.php"); //include the closing tags and JS files
